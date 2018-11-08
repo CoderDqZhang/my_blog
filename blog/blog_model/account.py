fb4e3f0a98f77d760b2cd24da91efd1f8488ff6a
CoderDqZhang <769839948@qq.com>
2018-11-08 10:31:08 +0800

    ui change

--- blog/blog_model/account.py
+++ blog/blog_model/account.py
@@ -8,7 +8,7 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField('用户名', max_length=200)  # 用户名
     openid = models.CharField(max_length=200, primary_key=True)
-    avatar = models.ImageField('头像', upload_to="avatar/%Y/%m", default=u"image/default.png", max_length=200, null=True)
+    avatar = models.ImageField('头像', upload_to="avatar/%Y/%m", default=u"image/default.png", blank=True, null=True)
     createTime = models.DateField(auto_created=True, auto_now_add=True)
     gender = models.IntegerField('性别', choices=define.GENDER, default=0, null=True)
     phone = models.CharField('电话', max_length=11, default='', null=True)
